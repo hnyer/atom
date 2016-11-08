8599698f77d0f7598910ee55b68522824013fcde
Damien Guard <damieng@gmail.com>
2016-11-08 12:18:05 -0800

    Provide x64 and x86 Windows RELEASES

--- script/lib/create-windows-installer.js
+++ script/lib/create-windows-installer.js
@@ -17,7 +17,7 @@ module.exports = function (packagedAppPath, codeSign) {
     iconUrl: `https://raw.githubusercontent.com/atom/atom/master/resources/app-icons/${CONFIG.channel}/atom.ico`,
     loadingGif: path.join(CONFIG.repositoryRootPath, 'resources', 'win', 'loading.gif'),
     outputDirectory: CONFIG.buildOutputPath,
-    remoteReleases: `https://atom.io/api/updates?version=${CONFIG.appMetadata.version}`,
+    remoteReleases: `https://atom.io/api/updates${process.arch === 'x64' ? '-x64' : ''}`,
     setupIcon: path.join(CONFIG.repositoryRootPath, 'resources', 'app-icons', CONFIG.channel, 'atom.ico')
   }
 
