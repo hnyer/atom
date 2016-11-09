4740d4ccdb43439a4fc56752267010545d06d319
Antonio Scandurra <as-cii@github.com>
2016-11-09 13:04:42 +0100

    :art:

--- script/lib/generate-snapshot-script.js
+++ script/lib/generate-snapshot-script.js
@@ -40,137 +40,6 @@ module.exports = function () {
     )
   }
 
-  function isReferenced (node, parent) {
-    switch (parent.type) {
-      // yes: object::NODE
-      // yes: NODE::callee
-      case "BindExpression":
-        return parent.object === node || parent.callee === node;
-
-      // yes: PARENT[NODE]
-      // yes: NODE.child
-      // no: parent.NODE
-      case "MemberExpression":
-      case "JSXMemberExpression":
-        if (parent.property === node && parent.computed) {
-          return true;
-        } else if (parent.object === node) {
-          return true;
-        } else {
-          return false;
-        }
-
-      // no: new.NODE
-      // no: NODE.target
-      case "MetaProperty":
-        return false;
-
-      // yes: { [NODE]: "" }
-      // yes: { NODE }
-      // no: { NODE: "" }
-      case "ObjectProperty":
-        if (parent.key === node) {
-          return parent.computed;
-        }
-
-      // no: let NODE = init;
-      // yes: let id = NODE;
-      case "VariableDeclarator":
-        return parent.id !== node;
-
-      // no: function NODE() {}
-      // no: function foo(NODE) {}
-      case "ArrowFunctionExpression":
-      case "FunctionDeclaration":
-      case "FunctionExpression":
-        for (let param of parent.params) {
-          if (param === node) return false;
-        }
-
-        return parent.id !== node;
-
-      // no: export { foo as NODE };
-      // yes: export { NODE as foo };
-      // no: export { NODE as foo } from "foo";
-      case "ExportSpecifier":
-        if (parent.source) {
-          return false;
-        } else {
-          return parent.local === node;
-        }
-
-      // no: export NODE from "foo";
-      // no: export * as NODE from "foo";
-      case "ExportNamespaceSpecifier":
-      case "ExportDefaultSpecifier":
-        return false;
-
-      // no: <div NODE="foo" />
-      case "JSXAttribute":
-        return parent.name !== node;
-
-      // no: class { NODE = value; }
-      // yes: class { [NODE] = value; }
-      // yes: class { key = NODE; }
-      case "ClassProperty":
-        if (parent.key === node) {
-          return parent.computed;
-        } else {
-          return parent.value === node;
-        }
-
-      // no: import NODE from "foo";
-      // no: import * as NODE from "foo";
-      // no: import { NODE as foo } from "foo";
-      // no: import { foo as NODE } from "foo";
-      // no: import NODE from "bar";
-      case "ImportDefaultSpecifier":
-      case "ImportNamespaceSpecifier":
-      case "ImportSpecifier":
-        return false;
-
-      // no: class NODE {}
-      case "ClassDeclaration":
-      case "ClassExpression":
-        return parent.id !== node;
-
-      // yes: class { [NODE]() {} }
-      case "ClassMethod":
-      case "ObjectMethod":
-        return parent.key === node && parent.computed;
-
-      // no: NODE: for (;;) {}
-      case "LabeledStatement":
-        return false;
-
-      // no: try {} catch (NODE) {}
-      case "CatchClause":
-        return parent.param !== node;
-
-      // no: function foo(...NODE) {}
-      case "RestElement":
-        return false;
-
-      // yes: left = NODE;
-      // no: NODE = right;
-      case "AssignmentExpression":
-        return parent.right === node;
-
-      // no: [NODE = foo] = [];
-      // yes: [foo = NODE] = [];
-      case "AssignmentPattern":
-        return parent.right === node;
-
-      // no: [NODE] = [];
-      // no: ({ NODE }) = [];
-      case "ObjectPattern":
-      case "ArrayPattern":
-        return false;
-    }
-
-    return true;
-  }
-
   function isReferenceToLazyRequire (path) {
     const node = path.node
     const parent = path.parent.node
@@ -184,7 +53,7 @@ module.exports = function () {
 
   function replaceAssignmentOrDeclarationWithLazyFunction (path) {
     let parentPath = path.parent
-    while (parentPath != null) {
+    while (parentPath != null && parentPath.scope === path.scope) {
       const parentNode = parentPath.node
       if (parentNode.type === 'AssignmentExpression') {
         const lazyRequireFunctionName = `get_${parentNode.left.name}`
@@ -194,7 +63,7 @@ module.exports = function () {
             b.assignmentExpression('=', parentNode.left, b.logicalExpression('||', parentNode.left, parentNode.right))
           )
         ])))
-        break
+        return
       } else if (parentNode.type === 'VariableDeclarator') {
         const lazyRequireFunctionName = `get_${parentNode.id.name}`
         const variableDeclarationPath = parentPath.parent
@@ -209,17 +78,12 @@ module.exports = function () {
             b.assignmentExpression('=', parentNode.id, b.logicalExpression('||', parentNode.id, parentNode.init))
           )
         ])))
-        break
+        return
       }
       parentPath = parentPath.parent
     }
 
-    if (parentPath == null) {
-      throw new Error(`
-        Using a blacklisted module for its side effects in this file's topmost scope.
-        Consider moving it inside an initialization function to call at runtime.
-      `)
-    }
+    throw new Error('The supplied node does not belong to an assignment expression or a variable declaration!')
   }
 
   recast.types.visit(ast, {
@@ -252,4 +116,135 @@ module.exports = function () {
 
   // console.log(lazyRequireFunctionsByVariableName);
   console.log(recast.print(ast).code);
+}
+
+function isReferenced (node, parent) {
+  switch (parent.type) {
+    // yes: object::NODE
+    // yes: NODE::callee
+    case "BindExpression":
+      return parent.object === node || parent.callee === node;
+
+    // yes: PARENT[NODE]
+    // yes: NODE.child
+    // no: parent.NODE
+    case "MemberExpression":
+    case "JSXMemberExpression":
+      if (parent.property === node && parent.computed) {
+        return true;
+      } else if (parent.object === node) {
+        return true;
+      } else {
+        return false;
+      }
+
+    // no: new.NODE
+    // no: NODE.target
+    case "MetaProperty":
+      return false;
+
+    // yes: { [NODE]: "" }
+    // yes: { NODE }
+    // no: { NODE: "" }
+    case "ObjectProperty":
+      if (parent.key === node) {
+        return parent.computed;
+      }
+
+    // no: let NODE = init;
+    // yes: let id = NODE;
+    case "VariableDeclarator":
+      return parent.id !== node;
+
+    // no: function NODE() {}
+    // no: function foo(NODE) {}
+    case "ArrowFunctionExpression":
+    case "FunctionDeclaration":
+    case "FunctionExpression":
+      for (let param of parent.params) {
+        if (param === node) return false;
+      }
+
+      return parent.id !== node;
+
+    // no: export { foo as NODE };
+    // yes: export { NODE as foo };
+    // no: export { NODE as foo } from "foo";
+    case "ExportSpecifier":
+      if (parent.source) {
+        return false;
+      } else {
+        return parent.local === node;
+      }
+
+    // no: export NODE from "foo";
+    // no: export * as NODE from "foo";
+    case "ExportNamespaceSpecifier":
+    case "ExportDefaultSpecifier":
+      return false;
+
+    // no: <div NODE="foo" />
+    case "JSXAttribute":
+      return parent.name !== node;
+
+    // no: class { NODE = value; }
+    // yes: class { [NODE] = value; }
+    // yes: class { key = NODE; }
+    case "ClassProperty":
+      if (parent.key === node) {
+        return parent.computed;
+      } else {
+        return parent.value === node;
+      }
+
+    // no: import NODE from "foo";
+    // no: import * as NODE from "foo";
+    // no: import { NODE as foo } from "foo";
+    // no: import { foo as NODE } from "foo";
+    // no: import NODE from "bar";
+    case "ImportDefaultSpecifier":
+    case "ImportNamespaceSpecifier":
+    case "ImportSpecifier":
+      return false;
+
+    // no: class NODE {}
+    case "ClassDeclaration":
+    case "ClassExpression":
+      return parent.id !== node;
+
+    // yes: class { [NODE]() {} }
+    case "ClassMethod":
+    case "ObjectMethod":
+      return parent.key === node && parent.computed;
+
+    // no: NODE: for (;;) {}
+    case "LabeledStatement":
+      return false;
+
+    // no: try {} catch (NODE) {}
+    case "CatchClause":
+      return parent.param !== node;
+
+    // no: function foo(...NODE) {}
+    case "RestElement":
+      return false;
+
+    // yes: left = NODE;
+    // no: NODE = right;
+    case "AssignmentExpression":
+      return parent.right === node;
+
+    // no: [NODE = foo] = [];
+    // yes: [foo = NODE] = [];
+    case "AssignmentPattern":
+      return parent.right === node;
+
+    // no: [NODE] = [];
+    // no: ({ NODE }) = [];
+    case "ObjectPattern":
+    case "ArrayPattern":
+      return false;
+  }
+
+  return true;
 }
